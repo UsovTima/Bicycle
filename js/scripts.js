2bfc97ec06d3ea64145cb044f784baea6f2779bf
UsovTima <timausov123@gmail.com>
2021-11-29 19:59:58 +0200

    Home work 9 with description of work

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-   
+   $('.menu-button').on('click', function() {
+   	$('.navigation-wrapper').addClass('active');
+   })
+
+   $('.close-menu').on('click', function() {
+   	$('.navigation-wrapper').removeClass('active');
+   })
 })
